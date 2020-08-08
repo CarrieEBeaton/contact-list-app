28b54aaeb9b3ea2537468411ecc15c79a3f49b44
Carrie Beaton <carrie.beaton@outlook.com>
2020-08-07 23:10:10 -0400

    add unit tests contact edit form

--- src/app/contacts/components/contact-edit/contact-edit.component.spec.ts
+++ src/app/contacts/components/contact-edit/contact-edit.component.spec.ts
@@ -1,9 +1,7 @@
-import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
-
-import { ContactEditComponent } from './contact-edit.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { DebugElement } from '@angular/core';
-import { By } from '@angular/platform-browser';
+import { async, ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ContactEditComponent } from './contact-edit.component';
 
 describe('ContactEditComponent', () => {
   let component: ContactEditComponent;
@@ -29,16 +27,10 @@ describe('ContactEditComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  fit('should emit create on save click', fakeAsync(() => {
+  it('should have valid forms with valid data', () => {
     //Arrange
-    const createContact = spyOn(component, 'saveContact');
     let createBtn = debugElement.nativeElement.querySelector('#createBtn');
-    component.contactForm.controls['firstName'].setValue('firstName');
-    component.contactForm.controls['lastName'].setValue('lastName');
-    component.contactForm.controls['company'].setValue('company');
-    component.contactForm.controls['email'].setValue('email@email.com');
-    component.contactForm.controls['phone'].setValue('458-123-1234');
-    component.contactForm.controls['address'].setValue('address');
+    setForm();
     fixture.detectChanges();
 
     createBtn.click();
@@ -52,6 +44,53 @@ describe('ContactEditComponent', () => {
     expect(component.contactForm.controls['address'].valid).toBeTruthy();
     expect(component.contactForm.valid).toBeTruthy();
     expect(createBtn.disabled).toBeFalsy();
+  });
+
+  it('should have invalid property with invalid data for required fields', () => {
+    //Arrange
+    component.contactForm.controls['firstName'].setValue('');
+    fixture.detectChanges();
+    //Assert
+    expect(component.contactForm.controls['firstName'].valid).toBeFalsy();
+
+  });
+
+  it('should have invalid property with invalid data for email format', () => {
+    //Arrange
+    component.contactForm.controls['email'].setValue('e.com');
+    fixture.detectChanges();
+    //Assert
+    expect(component.contactForm.controls['email'].valid).toBeFalsy();
+
+  });
+
+  it('should call save Contact on click of save', () => {
+    //Arrange
+    const createContact = spyOn(component, 'saveContact');
+    let createBtn = debugElement.nativeElement.querySelector('#createBtn');
+    setForm();
+    fixture.detectChanges();
+    createBtn.click();
+
+    //Assert
+    expect(component.contactForm.controls['firstName'].valid).toBeTruthy();
+    expect(component.contactForm.controls['lastName'].valid).toBeTruthy();
+    expect(component.contactForm.controls['company'].valid).toBeTruthy();
+    expect(component.contactForm.controls['email'].valid).toBeTruthy();
+    expect(component.contactForm.controls['phone'].valid).toBeTruthy();
+    expect(component.contactForm.controls['address'].valid).toBeTruthy();
+    expect(component.contactForm.valid).toBeTruthy();
+    expect(createBtn.disabled).toBeFalsy();
     expect(createContact).toHaveBeenCalled()
-  }));
+  });
+
+  function setForm() {
+    component.contactForm.controls['firstName'].setValue('firstName');
+    component.contactForm.controls['lastName'].setValue('lastName');
+    component.contactForm.controls['company'].setValue('company');
+    component.contactForm.controls['email'].setValue('email@email.com');
+    component.contactForm.controls['phone'].setValue('458-123-1234');
+    component.contactForm.controls['address'].setValue('address');
+  }
+
 });
